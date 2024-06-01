c69f9de5c042a3e363c05366b3268466d827c074
surbhisamota10 <95540023+surbhisamota10@users.noreply.github.com>
2024-06-01 16:37:31 +0530

    Update main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -1,1 +1,19 @@
 
+variable "ami_value" {
+  description = "value of the ami "
+}
+variable "instance_type_value" {
+  description = " value for instance type"
+}
+variable "subnet_id_value" {
+  description = "value for the subnet id"
+}
+provider "aws" {
+    region = "us-east-1"  
+}
+resource "aws_instance" "example" {
+  ami = "var.ami_value"
+  instance_type = "var.instance_type_value"
+  subnet_id = "var.subnet_id_value"
+}
+
